b5ae1a9d03e7b393b764752d74faf3d13d4aa218
rsatnarain <type.unique_0o@icloud.com>
2026-04-29 16:26:01 -0400

    Fix: Correct formatting in outputs.tf and enhance descriptions for clarity on CloudFront distribution ID

--- terraform_nextjs/outputs.tf
+++ terraform_nextjs/outputs.tf
@@ -4,7 +4,8 @@
 #
 # Updated By     Update Date   Version   Description
 # -----------------------------------------------------------------------------------------------
-# Rob.           2026-04-27.    1.0.     Initial Create
+# Rob            2026-04-27.    1.0.     Initial Create
+# Rob            2026-04-29.    1.1.     Fix: Added output for CloudFront distribution ID to facilitate cache invalidation and updated descriptions for clarity. This enhancement allows users to easily identify the CloudFront distribution associated with the S3 bucket, making it more convenient to manage and invalidate caches when necessary, ensuring that updates to the website are reflected promptly for end-users. Cache invalidation is crucial for maintaining the performance and freshness of the content served through CloudFront, especially after updates to the S3 bucket content. Handled using a CI/CD pipeline via GitHub Actions, the distribution ID is essential for targeting the correct distribution during cache invalidation processes.
 
 output "cloudfront_domain_name" {
   value       = aws_cloudfront_distribution.website_distribution.domain_name
@@ -14,4 +15,9 @@ output "cloudfront_domain_name" {
 output "s3_bucket_name" {
   value       = aws_s3_bucket.website_bucket.id
   description = "Name of the S3 bucket"
+}
+
+output "cloudfront_distribution_id" {
+  value       = aws_cloudfront_distribution.website_distribution.id
+  description = "The ID of the CloudFront distribution for cache invalidation"
 }
